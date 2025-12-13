3278a6fe9c76756e65fdd4f1e207a83151a0a2bb
Bárdonicsek Róbert <rbardo@gmail.com>
2025-12-13 12:09:20 +0100

    Név és konyvtár refactor , " Collection definiálása

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import './App.css';
 import HomePage from './pages/homepage/home-page.component.jsx';
-import { Route, Router  } from 'react-router-dom';
+import ShopPage from './pages/shop/shop.component.jsx';
+import { Route, Routes, useParams  } from 'react-router-dom';
+
+const Teszt = () => {
+  const params = useParams();
+  console.log(params);
+  return <h1>Teszt oldal</h1>;
+}
+
 
 const App = () => {
   return (
     <div>
-      <Switch>
-        <Route exact path='/' component={HomePage} />        
-      </Switch>      
+      <Routes>
+        <Route exact path='/' element={ <HomePage />} />
+        <Route path='/shop' element={ <ShopPage />} />      
+      </Routes>      
     </div>
   );
 }
