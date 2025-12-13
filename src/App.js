d1c84fb0f2d94f54bdb35984d5d6bdad59c6e5d0
Bárdonicsek Róbert <rbardo@gmail.com>
2025-12-13 13:55:48 +0100

    Add Header component and styles to the application

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Header from './components/header/header.component.jsx';
 import HomePage from './pages/homepage/home-page.component.jsx';
 import ShopPage from './pages/shop/shop.component.jsx';
 import { Route, Routes, useParams  } from 'react-router-dom';
@@ -13,6 +14,7 @@ const Teszt = () => {
 const App = () => {
   return (
     <div>
+      <Header />  
       <Routes>
         <Route exact path='/' element={ <HomePage />} />
         <Route path='/shop' element={ <ShopPage />} />      
