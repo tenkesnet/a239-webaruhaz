6d5e9a235c2b54cd8e65124a3f7d88f804f2e90e
Bárdonicsek Róbert <rbardo@gmail.com>
2026-01-09 23:24:25 +0100

    cart elem hozzáadása (redux,dropdown,icon)

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import SHOP_DATA from './shop.data.js';
 
-import CollectionPreview from '../../components/collection-preview/collection-preview.component.jsx';
+import CollectionPreview from '../../components/collection-preview/collection-preview.jsx';
 
 
 class ShopPage extends React.Component {
