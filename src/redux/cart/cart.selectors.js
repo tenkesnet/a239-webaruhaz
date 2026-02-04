b88fcd22dcb9439fa77a13b1281f1775db800fc2
Bárdonicsek Róbert <rbardo@gmail.com>
2026-02-04 18:34:27 +0100

    CartTotal javítás

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -26,8 +26,8 @@ export const selectCartTotal = createSelector(
   [selectCartItems],
   cartItems =>
     cartItems.reduce(
-      (accumalatedQuantity, cartItem) =>
-        accumalatedQuantity + cartItem.quantity * cartItem.price,
+      (accumulatedPrice, cartItem) =>
+        accumulatedPrice + cartItem.quantity * cartItem.price,
       0
     )
 );
